c885e90f844a64b061b3acd18c62731fcb6f620b
itstumpa <tumpadas1fiverr@gmail.com>
2026-04-07 14:59:10 +0600

    update ci.yml, jest.config.js and package.json

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
-  globals: { 'ts-jest': { tsconfig: { strict: false } } },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: { strict: false } }],
+  },
+  setupFiles: ['dotenv/config'],
 };
